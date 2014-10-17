c550da9302dcfe36b7e0f4e03d5442855f809bff
Andrei Zhlobich <a.zhlobich@gmail.com>
2014-10-17 20:44:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# pintoid
+The Game.
+
+## License
+
+Copyright © 2014 pint-of-code
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,40 @@
+(defproject pintoid "0.1.0-SNAPSHOT"
+
+  :description "Multiplayer asteroid-like game"
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [ring/ring-core "1.3.1"]
+                 [jarohen/chord "0.4.2"]
+                 [compojure/compojure "1.2.0"]
+                 [hiccup/hiccup "1.0.5"]
+                 [prismatic/dommy "0.1.3"]]
+
+  :plugins [[lein-pdo "0.1.1"]
+            [lein-cljsbuild "1.0.3"]
+            [lein-shell "0.4.0"]
+            [jarohen/lein-frodo "0.4.1"]]
+
+  ;; config
+  :frodo/config-resource "conf/development.edn"
+
+  ;; commands
+  :aliases {"dev" ["do"
+                   ["shell" "mkdir" "-p" "target/resources"]
+                   ["pdo" ["cljsbuild" "auto"] "frodo"]]}
+
+  :source-paths ["src"]
+  :resource-paths ["resources" "target/resources"]
+
+  ;; clojurescript
+  :cljsbuild
+  {
+   :builds
+   [{:source-paths ["src"]
+     :compiler {:output-to "target/resources/js/myfaga_proto.js"
+                :optimizations :whitespace
+                :pretty-print true
+                :externs ["resources/js/pixi.js"]
+                }}]}
+  )
