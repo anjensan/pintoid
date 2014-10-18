b2dc47d4c8b64afb4fa816e64824890e7141c3ba
wheercool <wheercool@gmail.com>
2014-10-18 16:37:17 +0300

    Added function for user input state detection

--- src/pintoid/client/uinput.cljs
+++ src/pintoid/client/uinput.cljs
@@ -10,15 +10,70 @@
    ))
 
 
-(defn handle-keydown [e]
-  (log :debug "key event" e)
-  (case (.-keyCode e)
-    37 (println "pressed left")
-    38 (println "pressed up")
-    39 (println "pressed right")
-    40 (println "pressed down")
-    nil))
       
+(def active-keys (array))
+
+
+(defn handle-keydown [e]
+  (let [
+    evt (or e (.event js/window))
+    char-code (or (.-keyCode evt) (.-which evt))
+    ]
+    (when (< (.indexOf active-keys char-code) 0) 
+      (.push active-keys char-code))
+      (println active-keys)
+    ))
+  ; (log :debug "key event" e)
+  ; (case (.-keyCode e)
+  ;   37 (println "pressed left")
+  ;   38 (println "pressed up")
+  ;   39 (println "pressed right")
+  ;   40 (println "pressed down")
+  ;   nil))
+      
+(defn handle-keyup [e]
+  (let [
+    evt (or e (.event js/window))
+    char-code (or (.-keyCode evt) (.-which evt))
+    new-active-keys (.filter active-keys (fn [key] (not= key char-code)) )
+    ]
+    (set! active-keys new-active-keys)
+    )
+)
+  ; (log :debug "key event" e)
+  ; (case (.-keyCode e)
+  ;   37 (println "pressed left")
+  ;   38 (println "pressed up")
+  ;   39 (println "pressed right")
+  ;   40 (println "pressed down")
+  ;   nil))
 
 (defn init-user-input []
-  (.addEventListener js/window "keydown" handle-keydown true))
+      (.addEventListener js/window "keydown" handle-keydown false)
+      (.addEventListener js/window "keyup" handle-keyup false)
+      (fn [] active-keys)
+)
+(def angle 0)
+
+(defn key-pressed? [char-code] 
+  (let [
+      f (>= (.indexOf active-keys char-code) 0)
+      ; _ (println f)
+    ]
+  f
+
+))
+
+(defn user-input-state []
+  (let [
+      space 32
+      arrow-left 37
+      arrow-right 39
+      rotate-step (/ (.-PI js/Math) 30)
+    ]
+    (when (key-pressed? arrow-left) (set! angle (- angle rotate-step)))
+    (when (key-pressed? arrow-right) (set! angle (+ angle rotate-step)))
+    { :angle angle
+      :engine-one?: (key-pressed? space) 
+    }
+    ))
