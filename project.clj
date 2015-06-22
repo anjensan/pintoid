91bdb8b5318467e14cb08ae441159efc54ff4507
Andrei Zhlobich <a.zhlobich@gmail.com>
2015-06-22 12:48:42 +0300

    UPGRADE deps

--- project.clj
+++ project.clj
@@ -2,22 +2,22 @@
 
   :description "Multiplayer asteroid-like game"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/clojurescript "0.0-2511"]
-                 [org.clojure/data.int-map "0.1.0"]
+                 [org.clojure/clojurescript "0.0-3308"]
+                 [org.clojure/data.int-map "0.2.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [ring/ring-core "1.3.2"]
-                 [jarohen/chord "0.5.0"]
-                 [compojure/compojure "1.3.1"]
+                 [jarohen/chord "0.6.0"]
+                 [compojure/compojure "1.3.4"]
                  [hiccup/hiccup "1.0.5"]
-                 [prismatic/dommy "1.0.0"]
-                 [ch.qos.logback/logback-classic "1.1.2"]]
+                 [prismatic/dommy "1.1.0"]
+                 [ch.qos.logback/logback-classic "1.1.3"]]
 
   :plugins [[lein-pdo "0.1.1"]
-            [lein-cljsbuild "1.0.3"]
+            [lein-cljsbuild "1.0.6"]
             [lein-shell "0.4.0"]
-            [jarohen/lein-frodo "0.4.1"]]
+            [jarohen/lein-frodo "0.4.2"]]
 
   ;; config
   :frodo/config-resource "conf/development.edn"
