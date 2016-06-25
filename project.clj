093fe02ca7013f36bf28e4fc9b8828c81592f824
Andrei Zhlobich <a.zhlobich@gmail.com>
2016-06-25 20:16:36 +0200

    Upgrade deps

--- project.clj
+++ project.clj
@@ -2,25 +2,25 @@
 
   :description "Multiplayer asteroid-like game"
 
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.clojure/clojurescript "0.0-3308"]
-                 [org.clojure/data.int-map "0.2.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.385"]
+                 [org.clojure/clojurescript "1.9.76"]
+                 [org.clojure/data.int-map "0.2.2"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [ring/ring-core "1.3.2"]
-                 [jarohen/chord "0.6.0"]
-                 [compojure/compojure "1.3.4"]
+                 [cljsjs/pixi "3.0.10-0"]
+                 [ring/ring-core "1.5.0"]
+                 [jarohen/chord "0.7.0"]
+                 [compojure/compojure "1.5.1"]
                  [hiccup/hiccup "1.0.5"]
                  [prismatic/dommy "1.1.0"]
-                 [ch.qos.logback/logback-classic "1.1.3"]
-                 [net.mikera/vectorz-clj "0.30.1"]
-                 [net.mikera/core.matrix "0.36.1"]
-                 [criterium "0.4.3"]
+                 [ch.qos.logback/logback-classic "1.1.7"]
                  ]
 
+  :profiles {:bench {:dependencies [[criterium/criterium "0.4.4"]]}}
+
   :plugins [[lein-pdo "0.1.1"]
-            [lein-cljsbuild "1.0.6"]
-            [lein-shell "0.4.0"]
+            [lein-cljsbuild "1.1.3"]
+            [lein-shell "0.5.0"]
             [jarohen/lein-frodo "0.4.2"]]
 
   ;; config
@@ -28,10 +28,9 @@
   ;; :frodo/config-resource "conf/production.edn"
 
   :jvm-opts ^:replace []
-  
+
   ;; commands
   :aliases {"dev" ["do"
-                   ["shell" "mkdir" "-p" "target/resources"]
                    ["pdo" ["cljsbuild" "auto"] "frodo"]]}
 
   :source-paths ["src"]
@@ -45,6 +44,5 @@
      :compiler {:output-to "target/resources/js/pintoid.js"
                 :optimizations :whitespace
                 :pretty-print true
-                :externs ["resources/js/pixi.js"]
                 }}]}
   )
