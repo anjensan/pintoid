8a87b6e993083309360553650c06fab0c7f4e285
Andrei Zhlobich <a.zhlobich@gmail.com>
2016-06-25 20:16:36 +0200

    Add cljs repl support into project.clj

--- project.clj
+++ project.clj
@@ -21,8 +21,10 @@
             [lein-shell "0.5.0"]
             [jarohen/lein-frodo "0.4.2"]]
 
-  :profiles
-  {:bench {:dependencies [[criterium/criterium "0.4.4"]]}}
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]
+                                  [org.clojure/tools.nrepl "0.2.12"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
+             :bench {:dependencies [[criterium/criterium "0.4.4"]]}}
 
   :hooks [leiningen.cljsbuild]
 
