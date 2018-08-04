9c5b43adfbecec36c7c5f391e64ef8f3c23159c8
Andrei Zhlobich <a.zhlobich@gmail.com>
2018-08-05 00:09:21 +0200

    Refactor server.core

--- src/pintoid/server/core.clj
+++ src/pintoid/server/core.clj
@@ -14,7 +14,6 @@
    [java.util.concurrent TimeUnit ScheduledThreadPoolExecutor]))
 
 
-
 (defstate config
   :start (if-let [cf (:config-file (mount/args))]
            (do
@@ -23,59 +22,41 @@
            (timbre/warn "No configuration file provided - start with all defaults")))
 
 
-(defmacro ^:private do-retfst [res & body]
-  `(let [r# ~res] ~@body r#))
-
-
 (defstate nrepl
   :start (let [ip (get-in config [:nrepl :bind] "127.0.0.1")
                port (get-in config [:nrepl :port] 9891)]
-           (timbre/debug "Starting nrepl server...")
-           (do-retfst
-            (nrepl/start-server
-             :bind ip :port port
-             :init-ns 'pintoid.dev)
-            (timbre/infof "Started nrepl server, port %s" port)))
-  :stop (do-retfst
-         (nrepl/stop-server nrepl)
-         (timbre/debug "Stopped nrepl server")))
+           (timbre/infof "Start nrepl server at port %s" port)
+           (nrepl/start-server :bind ip :port port
+                               :init-ns 'pintoid.dev))
+  :stop (do
+          (timbre/info "Stop nrepl server")
+          (nrepl/stop-server nrepl)))
 
 
 (defstate scheduler
   :start (let [th (get-in config [:scheduler :threads] 2)]
-           (timbre/debug "Starting scheduled thread pool executor...")
-           (do-retfst
-            (ScheduledThreadPoolExecutor. th)
-            (timbre/infof "Started scheduler, %s threads" th)))
-  :stop (do-retfst
-         (.shutdown scheduler)
-         (timbre/infof "Stopped scheduler")))
+           (timbre/infof "Start scheduler with %s threads" th)
+           (ScheduledThreadPoolExecutor. th))
+  :stop (do
+          (timbre/info "Stop scheduler")
+          (.shutdown scheduler)))
 
+(defn- sched-at-fixed-rate [f period]
+  (.scheduleAtFixedRate scheduler f 0 (long period) java.util.concurrent.TimeUnit/MILLISECONDS))
 
-(defn- sched-at-fixed-rate [scheduler f period]
-  (.scheduleAtFixedRate
-   scheduler
-   f 0 (long period)
-   java.util.concurrent.TimeUnit/MILLISECONDS))
+(defstate game-timer
+  :start (let [gtick (get-in config [:scheduler :game-tickrate] 30)]
+           (timbre/infof "Start game timer, period %sms" gtick)
+           (sched-at-fixed-rate #'game-world-tick gtick))
+  :stop (do (timbre/info "Stop game timer")
+            (.cancel game-timer true)))
 
-
-(defstate tickers
-  :start (let [ctick (get-in config [:scheduler :client-tickrate] 60)
-               gtick (get-in config [:scheduler :game-tickrate] 30)]
-           (do-retfst
-            {:game (sched-at-fixed-rate
-                    scheduler
-                    #'game-world-tick
-                    gtick)
-             :client (sched-at-fixed-rate
-                      scheduler
-                      #'send-snapshots-to-all-clients
-                      ctick)}
-            (timbre/infof "Game tick %s, client tick %s" gtick ctick)))
-  :stop (do-retfst
-         (doseq [[_ t] tickers]
-           (.cancel t true))
-         (timbre/debug "Stopped tickers")))
+(defstate client-timer
+  :start (let [ctick (get-in config [:scheduler :client-tickrate] 60)]
+           (timbre/infof "Start client timer, period %sms" ctick)
+           (sched-at-fixed-rate #'send-snapshots-to-all-clients ctick))
+  :stop (do (timbre/info "Stop client timer")
+            (.cancel client-timer true)))
 
 
 (defn ring-handler []
@@ -83,23 +64,19 @@
        (ring.middleware.params/wrap-params)
        (ring.middleware.session/wrap-session)))
 
-
 (defstate webserver
   :start (let [port (get-in config [:web :port] 8080)
                ip (get-in config [:web :bind] "0.0.0.0")]
-           (timbre/debugf "Starting web server on port %s..." port)
-           (do-retfst
-            (hkit/run-server (ring-handler) {:ip ip :port port})
-            (timbre/infof "Started web server, port %s" port)))
-  :stop (do-retfst
-         (webserver)
-         (timbre/infof "Stopped web server")))
+           (timbre/infof "Start web server at port %s" port)
+           (hkit/run-server (ring-handler) {:ip ip :port port}))
+  :stop (do
+          (timbre/info "Stop web server")
+          (webserver)))
 
 
 (defn start
   ([]
-   (start
-    (System/getenv "PINTOID_CONFIG")))
+   (start (System/getenv "PINTOID_CONFIG")))
   ([cf]
    (->
     (mount/with-args {:config-file cf})
