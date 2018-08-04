be355ac20bf5628528f629447be500407681f7c0
Andrei Zhlobich <a.zhlobich@gmail.com>
2018-08-05 00:09:49 +0200

    Refactor server.cswiring

--- src/pintoid/server/cswiring.clj
+++ src/pintoid/server/cswiring.clj
@@ -26,7 +26,6 @@
     :host (:remote-addr req)
     :ws-channel (:ws-channel req)}))
 
-
 (defn destroy-avatar [avatar]
   (timbre/trace "Send destroy function to avatar" avatar)
   (send
@@ -35,7 +34,6 @@
      (timbre/debug "Destroy avatar" (:pid as))
      (close! (:ws-channel as))
      ::destroyed)))
-
 
 (defn send-to-client [pid message]
   (timbre/tracef "Send to %s: %s" pid message)
@@ -47,8 +45,30 @@
      avatar)))
 
 
-(declare handle-client-message)
-(declare on-client-disconnected)
+(defmulti handle-client-message
+  (fn [a eid message] (:command message)))
+
+(defmethod handle-client-message :default [a pid m]
+  (timbre/warnf "Unknown message from %s: m" pid m)
+  a)
+
+(defmethod handle-client-message :join-game [a pid m]
+  (timbre/infof "New player %s" pid)
+  (game-add-new-player pid)
+  a)
+
+(defmethod handle-client-message :user-input [a pid m]
+  (timbre/trace "user input player" m)
+  (game-process-user-input pid (:data m))
+  (assoc a :user-input (:data m)))
+
+
+(defn on-client-disconnected [eid]
+  (timbre/infof "Player %s disconnected" eid)
+  (game-remove-player eid)
+  (destroy-avatar (get @avatars eid))
+  (swap! avatars dissoc eid))
+
 
 (defn- spawn-wschan-reading-loop [eid ws-channel]
   (go-loop []
@@ -58,12 +78,9 @@
           (timbre/warnf "Client %s: %s" eid error))
         (when message
           (timbre/tracef "Receive from %s: %s" eid message)
-          (send
-           (get @avatars eid)
-           (fn [a] (or (handle-client-message eid message a) a))))
+          (send (get @avatars eid) handle-client-message eid message))
         (recur))
       (on-client-disconnected eid))))
-
 
 (defn new-client-connection [req]
   (let [eid (next-entity-id)
@@ -77,36 +94,6 @@
               (spawn-wschan-reading-loop eid (:ws-channel s)))))))
 
 
-;; == Handle client commands.
-
-(defn on-client-disconnected [eid]
-  (timbre/infof "Player %s disconnected" eid)
-  (game-remove-player eid)
-  (destroy-avatar (get @avatars eid))
-  (swap! avatars dissoc eid))
-
-
-(defmulti handle-client-message
-  (fn [eid message a] (:command message)))
-
-
-(defmethod handle-client-message :default [pid m a]
-  (timbre/warnf "Unknown message from %s: m" pid m)
-  a)
-
-
-(defmethod handle-client-message :join-game [pid m a]
-  (timbre/infof "New player %s" pid)
-  (game-add-new-player pid)
-  a)
-
-
-(defmethod handle-client-message :user-input [pid m a]
-  (timbre/trace "user input player" m)
-  (game-process-user-input pid (:data m))
-  (assoc a :user-input (:data m)))
-
-
 ;; == Send world patch
 
 (declare make-world-dumper)
@@ -118,19 +105,20 @@
             wd (make-world-dumper pid w)
             [ss' dump] (wd ss w)
             wpatch (into {} (map (fn [[c d]] [c (vec d)])) dump)]
-        (timbre/trace "send to" pid "wpatch" wpatch)
+        (timbre/tracef "send to %s wpatch %s" pid wpatch)
         (send-to-client
          pid
          {:server-time (System/currentTimeMillis)
+          :game-time at
           :command :wpatch
           :self pid
-          :game-time at
           :ecs wpatch})
-        (assoc a :actual-world w :ss-state ss'))
+        (assoc a
+               :actual-world w
+               :ss-state ss'))
       (do
-        (timbre/trace "player" pid "not in game" a (entity w pid))
+        (timbre/tracef "player %s not in game" (entity w pid))
         a))))
-
 
 (defn send-snapshots-to-all-clients []
   (let [[at w] (get-world)]
@@ -139,55 +127,41 @@
        (send-off a create-and-send-world-patch at w)))))
 
 
-;; == World snapshot
-
-(defn- point->vec [p]
-  (when p
-    ((juxt :x :y) p)))
-
-
-(defn emap [f c]
-  (eduction (map f) c))
-
-
-(defn econcat [c1 c2]
-  (eduction cat [c1 c2]))
-
-
-(defn merge-dumps2 [d1 d2]
-  (merge-with #(eduction cat [%1 %2]) d1 d2))
-
+;; == Dumpers
 
 (defn combine-dumpers* [dumpers]
   (fn [state w]
     (let [sds (map
                (fn [[c df]]
-                 (let [s (get state c), [s' d] (df s w)] [c s' d]))
+                 (let [s (get state c)
+                       [s' d] (df s w)]
+                   [{c s'} d]))
                dumpers)
-          states (into {} (map (fn [[c s d]] [c s])) sds)
-          dumps (reduce merge-dumps2 (map (fn [[c s d]] d) sds))
-          ]
+          states (into {} (map first sds))
+          dumps  (apply merge-with #(eduction cat [%1 %2]) (map second sds))]
     [states dumps])))
 
-
 (defmacro combine-dumpers [& dumpers]
+  "Combine several dumpers into one."
   `(combine-dumpers*
     ~(zipmap (repeatedly #(keyword (gensym "ecs_dumper"))) dumpers)))
 
-
-(defn map-val
-  ([f] (map (fn [[k v]] [k (f v)])))
-  ([f m] (into (empty m) (map-val f) m)))
-
-
-(defn cdump [& {cid :cid eids :eids mapf :map fkey :fkey fval :fval}]
-  (fn [{cm' :compmap d' :dump :or {d' {}, cm' {}} :as state} w]
+(defn dumper [& {cid :cid eids :eids mapf :convert feid :feid fval :filter}]
+  "Returns dumper function [state world] => [new-state {:cid1 [...], :cid2 [...]}]
+   Arguments:
+      :cid  component name (mandatory)
+      :eids bitset of entity ids
+      :feid filter eids
+      :filter filter pairs [eid value]
+      :convert convert component values
+  "
+  (fn [{cm' :compmap d' :dump :or {d' {}} :as state} w]
     (let [cm (component-map w cid)]
       (if (= cm' cm)
         [state {}]
         (let [changed? (fn [[eid s]] (not (when-let [s' (d' eid)] (= s s'))))
               ef (cond-> identity
-                   fkey (comp (filter fkey))
+                   feid (comp (filter feid))
                    true (comp (map (fn [eid] [eid (cm eid)])))
                    true (comp (filter changed?))
                    fval (comp (filter fval)))
@@ -198,30 +172,28 @@
                           (not (contains? d-add eid))
                           (or
                            (not (contains? eids eid))
-                           (and fkey (not (fkey eid)))
+                           (and feid (not (feid eid)))
                            (and fval (not (fval (find cm eid)))))))
               d-rem (into [] (comp (map key) (filter removed?)) d')
               d (as-> d' $ (apply dissoc $ d-rem) (into $ d-add))]
           [(assoc state :compmap cm :dump d)
-           {cid (econcat
-                 (eduction (map (fn [x] [x nil])) d-rem)
-                 (if mapf (eduction (map-val mapf) d-add) d-add))}])))))
-
-;; == Dumpers
-;; TODO: Game logic => move outside this ns.
+           {cid (eduction
+                 cat [(eduction (map (fn [x] [x nil])) d-rem)
+                      (if mapf
+                        (eduction (map (fn [[k v]] [k (mapf v)])) d-add)
+                        d-add)])}])))))
 
 (defn dumper-only-visible-entities [pid w]
   (let [player-xy (w pid :position)
         is-visible? #(when-let [f (w % :visible?)] (f w pid %))
         visible (into (im/dense-int-set) (filter is-visible?) (entity-ids w :position))]
     (combine-dumpers
-     (cdump :cid :type, :eids visible)
-     (cdump :cid :position, :map point->vec, :eids visible)
-     (cdump :cid :position-tts, :eids visible)
-     (cdump :cid :angle, :eids visible)
-     (cdump :cid :layer, :eids visible)
-     (cdump :cid :sprite, :eids visible))))
-
+     (dumper :cid :type, :eids visible)
+     (dumper :cid :position, :convert (juxt :x :y), :eids visible)
+     (dumper :cid :position-tts, :eids visible)
+     (dumper :cid :angle, :eids visible)
+     (dumper :cid :layer, :eids visible)
+     (dumper :cid :sprite, :eids visible))))
 
 (defn dumper-self-player [pid]
   (fn [sent w]
@@ -229,11 +201,10 @@
       [true nil]
       [true {:self-player [[pid true]]}])))
 
-
 (defn make-world-dumper [pid w]
   (combine-dumpers
    (dumper-only-visible-entities pid w)
-    (cdump :cid :score)
-    (cdump :cid :assets)
-    (dumper-self-player pid)
-    ))
+   (dumper :cid :score)
+   (dumper :cid :assets)
+   (dumper-self-player pid)
+   ))
